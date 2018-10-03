d921841c40ac4c1c910c75516437c4fb2b105cfc
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-10-03 12:07:19 +0530

    Update rover.ino

--- rover.ino
+++ rover.ino
@@ -3,14 +3,12 @@ Servo esc1,esc2,esc3,esc4; //Creating a servo class with name as esc
 
 void setup()
 {
-pinMode(4,INPUT);
-pinMode(5,INPUT);
-pinMode(6,INPUT);
-pinMode(7,INPUT);
-pinMode(8,OUTPUT);
-pinMode(9,OUTPUT);
+pinMode(12,INPUT);
+pinMode(13,INPUT);
+pinMode(3,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
 pinMode(11,OUTPUT);
-pinMode(12,OUTPUT);
 Serial.begin(9600);
 }
 int val1,val2,val3,val4;
@@ -18,14 +16,14 @@ int motor1,motor2,motor3,motor4;
 
 void getpulse()
 {
-val1= pulseIn(4,HIGH,25000);
+val1= pulseIn(12,HIGH,25000);
 Serial.println("channel 1");
 Serial.println(val1);//Read input from analog pin a0 and store in val
 /*val2= pulseIn(5,HIGH,25000);
 Serial.println("channel 2");
 Serial.println(val2);//Read input from analog pin a0 and store in val
 delay(1000);*/
-val3= pulseIn(6,HIGH,25000);//channel 3 throttle 
+val3= pulseIn(13,HIGH,25000);//channel 3 throttle 
 Serial.println("channel 3");
 Serial.println(val3);//Read input from analog pin a0 and store in val
 
@@ -37,35 +35,35 @@ delay(500);*/
 void forward()
 {
  
-  digitalWrite(8,1);
-  digitalWrite(9,0);
-  digitalWrite(11,1);
-  digitalWrite(12,0);
+  digitalWrite(3,1);
+  digitalWrite(5,0);
+  digitalWrite(6,1);
+  digitalWrite(11,0);
 }
 
 void back()
 {
-  digitalWrite(8,0);
-  digitalWrite(9,1);
-  digitalWrite(11,0);
-  digitalWrite(12,1);
+  digitalWrite(3,0);
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(11,1);
 }
 
 
 void left()
 {
-  digitalWrite(8,0);
-  digitalWrite(9,1);
-  digitalWrite(11,1);
-  digitalWrite(12,0);
+  digitalWrite(3,0);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(11,0);
 }
 
 void right()
 {
-  digitalWrite(8,1);
-  digitalWrite(9,0);
-  digitalWrite(11,0);
-  digitalWrite(12,1);
+  digitalWrite(3,1);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+  digitalWrite(11,1);
 }
 void loop()
 {
@@ -82,7 +80,7 @@ if (val3<1400)
 Serial.println("backward");
 digitalWrite(13,LOW);
 }
-if(val1>1500)
+if(val1>1600)
 {
 right();
 Serial.println("right");
